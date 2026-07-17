274eff3f078e0db3cbfd641bfaac53b85899bd78
Jeremiah Bedar <me@jbedar.com>
2026-07-17 13:12:02 -0500

    update to resolve github page to jbedar.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jbedar.github.io',
-  base: '/my-portfolio',
+  site: 'https://jbedar.com',
   integrations: [mdx(), sitemap(), tailwind()]
 });
